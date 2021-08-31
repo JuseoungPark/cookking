1dbd969e67172bfc6960bd3271208fad46dd7233
parkjuseoung <ehswmfql@gmail.com>
2021-08-31 20:12:05 +0900

    test

--- front/pages/Home.tsx
+++ front/pages/Home.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import Container from '../components/Container'
-import Button from '../components/Button'
+import { Btn, BtnCont } from '../components/Button'
+// import { Card } from '../components/Card'
 import { Text } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+
+import Input from '../components/TextInput'
 
 function Home() {
 	const navigation = useNavigation()
 	return (
 		<Container>
 			<Text>Home</Text>
-			<Button title="카운터로" onPress={() => navigation.navigate('Count', { title: '홈에서 눌렀다 임마!' })} />
+			<Btn onPress={() => navigation.navigate('Count', { title: '홈에서 눌렀다 임마!' })}>
+				<BtnCont>카운터로</BtnCont>
+			</Btn>
+
+			{/* <Card></Card> */}
+			<Input label="안녕" placeholder="ㅇㅇㅇ"></Input>
 		</Container>
 	)
 }
--- front/pages/Search.tsx
+++ front/pages/Search.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Container from '../components/Container'
-import Button from '../components/Button'
+import { Btn, BtnCont } from '../components/Button'
 import { Text } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 
@@ -9,7 +9,9 @@ function Search() {
 	return (
 		<Container>
 			<Text>Search</Text>
-			<Button title="카운터로" onPress={() => navigation.navigate('Count', { title: '서치에서 눌렀다 임마!' })} />
+			<Btn onPress={() => navigation.navigate('Count', { title: '서치에서 눌렀다 임마!' })}>
+				<BtnCont>카운터로</BtnCont>
+			</Btn>
 		</Container>
 	)
 }
